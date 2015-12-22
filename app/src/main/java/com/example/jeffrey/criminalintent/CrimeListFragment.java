63bde454ebbdd99aa56237e128f52f6cb4b073e5
Jeffrey Chang <jeffrey@Jeffreys-MacBook-Air.local>
2015-12-22 14:27:00 -0500

    Added proper touch listener

--- app/src/main/java/com/example/jeffrey/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/jeffrey/criminalintent/CrimeListFragment.java
@@ -6,10 +6,12 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Layout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -20,21 +22,38 @@ public class CrimeListFragment extends Fragment{
 
     private RecyclerView mCrimeRecyclerView;
     private CrimeAdapter mAdapter;
+    private Crime crime;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime_lost, container, false);
         mCrimeRecyclerView = (RecyclerView) v.findViewById(R.id.crime_recycler_view);
         mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        RecyclerView.Adapter adapter = mCrimeRecyclerView.getAdapter();
+        UpdateUI();
 
         return v;
     }
-    private class CrimeHolder extends RecyclerView.ViewHolder {
+    private void UpdateUI() {
+        CrimeLab crimeLab = CrimeLab.get(getActivity());
+        List<Crime> crimes = crimeLab.getCrimes();
+        mAdapter = new CrimeAdapter(crimes);
+        mCrimeRecyclerView.setAdapter(mAdapter);
+
+
+    }
+    private class CrimeHolder extends RecyclerView.ViewHolder implements  View.OnClickListener {
         public TextView mTitleTextView;
         public CrimeHolder(View itemView) {
             super(itemView);
-            mTitleTextView = (TextView) itemView;
+            itemView.setOnClickListener(this);
+            mTitleTextView = (TextView) itemView.findViewById(R.id.crime_name);
+
         }
+
+        public void onClick(View v) {
+
+            Toast.makeText(getActivity(),mTitleTextView.getText() + " clicked!",Toast.LENGTH_SHORT).show();
+        }
+
     }
     private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder> {
         private List<Crime> mCrimes;
@@ -46,25 +65,22 @@ public class CrimeListFragment extends Fragment{
         @Override
         public CrimeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
-            View view = layoutInflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+            View view = layoutInflater.inflate(R.layout.layout, parent, false);
             return new CrimeHolder(view);
         }
 
         @Override
+
         public void onBindViewHolder(CrimeHolder holder, int position) {
-            Crime crime = mCrimes.get(position);
+            crime = mCrimes.get(position);
             holder.mTitleTextView.setText(crime.getmTitle());
         }
         public int getItemCount() {
             return mCrimes.size();
         }
-        private void UpdateUI() {
-            CrimeLab crimeLab = CrimeLab.get(getActivity());
-            List<Crime> crimes = crimeLab.getCrimes()````;```````````
-            mAdapter = new CrimeAdapter(crimes);
-            mCrimeRecyclerView.setAdapter(mAdapter);
 
-
+        public Crime getCrime() {
+            return crime;
         }
     }
 }
